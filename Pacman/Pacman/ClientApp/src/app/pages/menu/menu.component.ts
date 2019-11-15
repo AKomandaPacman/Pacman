1aa4986aed9ba248b9cd179478e8c5b37e11f50e
Giedre Pranauskyte <giedre9723@gmail.com>
2019-11-16 00:42:39 +0200

    test2

--- Pacman/Pacman/ClientApp/src/app/pages/menu/menu.component.ts
+++ Pacman/Pacman/ClientApp/src/app/pages/menu/menu.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
 
-import { FormGroup, FormBuilder} from '@angular/forms';
+// import { FormGroup, FormBuilder} from '@angular/forms';
 
-@Component({
-  selector: 'xn-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.less']
-})
-export class MenuComponent implements OnInit {
+// @Component({
+  // selector: 'xn-menu',
+  // templateUrl: './menu.component.html',
+  // styleUrls: ['./menu.component.less']
+// })
+// export class MenuComponent implements OnInit {
 
-  inputv: FormGroup = this.builder.group({
-    email: [null],
-    password: [null],
-    remember: [true]
-  });
+  // inputv: FormGroup = this.builder.group({
+    // email: [null],
+    // password: [null],
+    // remember: [true]
+  // });
 
-  constructor(
-    private builder: FormBuilder) { }
+  // constructor(
+    // private builder: FormBuilder) { }
 
-  ngOnInit() {
-  }
+  // ngOnInit() {
+  // }
   
-  submit()
-  {
-  }
-}
+  // submit()
+  // {
+  // }
+// }
--- Pacman/Pacman/ClientApp/src/environments/environment.ts
+++ Pacman/Pacman/ClientApp/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+	production: true,
     backend_url: 'http://localhost:50924/api'
 };
 
